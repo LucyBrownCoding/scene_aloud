c2faa9e89953f2b395e475bc03611e72a67f0001
lucybrown <oreothedogisnumberonelol@gmail.com>
2024-12-30 14:26:14 -0600

    Initial Commit

---
+++ SceneAloud/SceneAloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  SceneAloudApp.swift
+//  SceneAloud
+//
+//  Created by Lucy Brown on 12/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SceneAloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SceneAloudTests/SceneAloudTests.swift
@@ -0,0 +1,16 @@
+//
+//  SceneAloudTests.swift
+//  SceneAloudTests
+//
+//  Created by Lucy Brown on 12/30/24.
+//
+
+import Testing
+
+struct SceneAloudTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
